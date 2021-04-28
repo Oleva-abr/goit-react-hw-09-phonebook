762043a652870b4a32c33a837fae73d3fdf5e9f4
Olena Abramova <olena.v.abramova@gmail.com>
2021-04-28 18:51:12 +0300

    add hooks to register form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,92 +1,12 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-// import PropTypes from 'prop-types';
 import style from './contactForm.module.css';
-// import { connect } from 'react-redux';
 import { contactsOperations, contactsSelectors } from '../../redux/contacts';
-
+//toast
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 toast.configure();
-
-// class ContactForm extends Component {
-//   static propTypes = {
-//     name: PropTypes.string,
-//     value: PropTypes.string,
-//   };
-//   state = {
-//     name: '',
-//     number: '',
-//   };
-
-//   handleInputChange = e => {
-//     const { name, value } = e.currentTarget;
-
-//     this.setState({ [name]: value });
-//   };
-
-//   submitContact = e => {
-//     e.preventDefault();
-//     const { name } = this.state;
-//     const { contacts, onSubmit } = this.props;
-//     if (contacts.some(elm => elm.name.toLowerCase() === name.toLowerCase())) {
-//       return toast(`${name} is already in contacts`);
-//     }
-
-//     onSubmit(this.state);
-//     this.reset();
-//   };
-
-//   reset = () => {
-//     this.setState({
-//       name: '',
-//       number: '',
-//     });
-//   };
-
-//   render() {
-//     return (
-//       <div className={style.formWrapper}>
-//         <form onSubmit={this.submitContact}>
-//           <label className={style.label}>
-//             Name
-//             <input
-//               className={style.input}
-//               type="name"
-//               name="name"
-//               value={this.state.name}
-//               onChange={this.handleInputChange}
-//             />
-//           </label>
-//           <label className={style.label}>
-//             Number
-//             <input
-//               className={style.input}
-//               type="tel"
-//               name="number"
-//               value={this.state.number}
-//               onChange={this.handleInputChange}
-//             />
-//           </label>
-//           <button className={style.btn} type="submit">
-//             Add contact
-//           </button>
-//         </form>
-//       </div>
-//     );
-//   }
-// }
-
-// const mapStateToProps = state => ({
-//   contacts: contactsSelectors.getAllContacts(state),
-// });
-
-// const mapDispatchToProps = dispatch => ({
-//   onSubmit: contact => dispatch(contactsOperations.addContact(contact)),
-// });
-
-// export default connect(mapStateToProps, mapDispatchToProps)(ContactForm); //
 
 export default function ContactForm() {
   const [name, setName] = useState('');
@@ -105,12 +25,12 @@ export default function ContactForm() {
   const submitContact = e => {
     e.preventDefault();
 
-    if (name === '') {
+    if (!name) {
       toast(`Enter name`);
       return;
     }
 
-    if (number === '') {
+    if (!number) {
       toast(`Enter number`);
       return;
     }
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,82 +1,149 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { authOperations } from '../../redux/auth';
+import React, { useState } from 'react';
+// import { connect } from 'react-redux';
+import { authOperations, authSelectors } from '../../redux/auth';
 import style from '../ContactForm/contactForm.module.css';
-
+import { useSelector, useDispatch } from 'react-redux';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 toast.configure();
 
-class RegisterForm extends Component {
-  state = {
-    name: '',
-    email: '',
-    password: '',
+export default function RegisterForm() {
+  const [name, setName] = useState('');
+  const changeName = e => {
+    setName(e.target.value);
+  };
+  const [email, setEmail] = useState('');
+  const changeEmail = e => {
+    setEmail(e.target.value);
   };
 
-  handleChange = ({ target: { name, value } }) => {
-    this.setState({ [name]: value });
+  const [password, setPassword] = useState('');
+  const changePassword = e => {
+    setPassword(e.target.value);
   };
 
-  handleSubmit = e => {
+  const dispatch = useDispatch();
+
+  const handleSubmit = e => {
     e.preventDefault();
-    const { name, email } = this.state;
 
-    if (!name) return toast('Please enter contact name');
-    if (!email) return toast('Please enter contact email');
-    this.props.onRegister(this.state);
+    if (!name || !email || !password) return toast('Please fill the form');
 
-    this.setState({ name: '', email: '', password: '' });
+    dispatch(authOperations.register({ name, email, password }));
+    setName('');
+    setEmail('');
+    setPassword('');
   };
+  return (
+    <div>
+      <h1>Register page</h1>
 
-  render() {
-    const { name, email, password } = this.state;
-
-    return (
-      <div>
-        <h1>Register page</h1>
-
-        <form
-          className={style.registerForm}
-          onSubmit={this.handleSubmit}
-          autoComplete="off"
-        >
-          <label className={style.label}>Name</label>
-          <input
-            className={style.input}
-            type="text"
-            name="name"
-            value={name}
-            onChange={this.handleChange}
-          />
-          <label className={style.label}>Email</label>
-          <input
-            className={style.input}
-            type="email"
-            name="email"
-            value={email}
-            onChange={this.handleChange}
-          />
-          <label className={style.label}>Password</label>
-          <input
-            className={style.input}
-            type="password"
-            name="password"
-            value={password}
-            onChange={this.handleChange}
-          />
-          <button className={style.btn} type="submit">
-            Register
-          </button>
-        </form>
-      </div>
-    );
-  }
+      <form
+        className={style.registerForm}
+        onSubmit={handleSubmit}
+        autoComplete="off"
+      >
+        <label className={style.label}>Name</label>
+        <input
+          className={style.input}
+          type="text"
+          name="name"
+          value={name}
+          onChange={changeName}
+        />
+        <label className={style.label}>Email</label>
+        <input
+          className={style.input}
+          type="email"
+          name="email"
+          value={email}
+          onChange={changeEmail}
+        />
+        <label className={style.label}>Password</label>
+        <input
+          className={style.input}
+          type="password"
+          name="password"
+          value={password}
+          onChange={changePassword}
+        />
+        <button className={style.btn} type="submit">
+          Register
+        </button>
+      </form>
+    </div>
+  );
 }
+// class RegisterForm extends Component {
+//   state = {
+//     name: '',
+//     email: '',
+//     password: '',
+//   };
 
-const mapDispatchToProps = {
-  onRegister: authOperations.register,
-};
+//   handleChange = ({ target: { name, value } }) => {
+//     this.setState({ [name]: value });
+//   };
 
-export default connect(null, mapDispatchToProps)(RegisterForm);
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     const { name, email } = this.state;
+
+//     if (!name) return toast('Please enter contact name');
+//     if (!email) return toast('Please enter contact email');
+//     this.props.onRegister(this.state);
+
+//     this.setState({ name: '', email: '', password: '' });
+//   };
+
+//   render() {
+//     const { name, email, password } = this.state;
+
+//     return (
+//       <div>
+//         <h1>Register page</h1>
+
+//         <form
+//           className={style.registerForm}
+//           onSubmit={this.handleSubmit}
+//           autoComplete="off"
+//         >
+//           <label className={style.label}>Name</label>
+//           <input
+//             className={style.input}
+//             type="text"
+//             name="name"
+//             value={name}
+//             onChange={this.handleChange}
+//           />
+//           <label className={style.label}>Email</label>
+//           <input
+//             className={style.input}
+//             type="email"
+//             name="email"
+//             value={email}
+//             onChange={this.handleChange}
+//           />
+//           <label className={style.label}>Password</label>
+//           <input
+//             className={style.input}
+//             type="password"
+//             name="password"
+//             value={password}
+//             onChange={this.handleChange}
+//           />
+//           <button className={style.btn} type="submit">
+//             Register
+//           </button>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
+
+// const mapDispatchToProps = {
+//   onRegister: authOperations.register,
+// };
+
+// export default connect(null, mapDispatchToProps)(RegisterForm);
